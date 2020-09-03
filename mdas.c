4902baf9382cd6fa5ec52df1d00c563b83f5f1df
Play Pause <70633052+benny1436@users.noreply.github.com>
2020-09-03 09:59:02 +0800

    Update mdas.c

--- mdas.c
+++ mdas.c
@@ -2,16 +2,16 @@
 int main()
 {
 int a,b,sum,prod,diff,quo;
-printf("Enter Two Integers:");
+printf("Enter Two Integers:\n");
 scanf("\n%d \n%d",&a,&b);
 sum=a+b;
 diff=a-b;
 prod=a*b;
 quo=a/b;
-printf("Sum is: %d",sum);
-printf("Difference is: %d",diff);
-printf("Product is: %d",prod);
-printf("Quotient is %d",quo);
+printf("Sum is: %d\n",sum);
+printf("Difference is: %d\n",diff);
+printf("Product is: %d\n",prod);
+printf("Quotient is %d\n",quo);
 return 0;
 }
 
